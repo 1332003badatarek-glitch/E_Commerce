e0c94407da3aa9865c0df0d04dc94a7b57ecec7c
Abdallah Tarek <badatarek1332003@gmail.com>
2026-04-09 10:25:43 +0200

    create secure storage , shared pref interface then cache helper class

--- lib/core/errors/error_handler.dart
+++ lib/core/errors/error_handler.dart
@@ -9,22 +9,28 @@ class ErrorHandler {
         case DioExceptionType.sendTimeout:
         case DioExceptionType.receiveTimeout:
         case DioExceptionType.connectionError:
-          return const NetworkFailure('Connection error please try again later');
-        
+          return const NetworkFailure(
+            'Connection error please try again later',
+          );
 
         case DioExceptionType.badResponse:
-          final message = error.response?.data['message'] ?? "Server error please try again later";
+          final message =
+              error.response?.data['message'] ??
+              "Server error please try again later";
           return ServerFailure(message, statusCode: error.response?.statusCode);
 
         case DioExceptionType.cancel:
           return const UnknownFailure("Request was cancelled");
 
-      
         case DioExceptionType.unknown:
           if (error.message != null && error.message!.contains('TypeError')) {
-             return const ParsingFailure("Data processing error (Parsing Failure)");
+            return const ParsingFailure(
+              "Data processing error (Parsing Failure)",
+            );
           }
-          return UnknownFailure(error.message ?? 'Unknown error please try again later');
+          return UnknownFailure(
+            error.message ?? 'Unknown error please try again later',
+          );
 
         default:
           return const UnknownFailure('Something went wrong, please try again');
---
+++ lib/core/helpers/cache_helper.dart
@@ -0,0 +1,60 @@
+import 'package:flutter_secure_storage/flutter_secure_storage.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+
+abstract class LocalStorage {
+  Future<bool> saveData({required String key, required dynamic value});
+  dynamic getData({required String key});
+  Future<bool> removeData({required String key}); 
+}
+
+
+abstract class SecureStorage {
+  Future<void> saveSecureData({required String key, required String value});
+  Future<String?> getSecureData(String key);
+  Future<void> removeSecureData({required String key}); 
+}
+
+class CacheHelper implements LocalStorage, SecureStorage {
+  final SharedPreferences _prefs;
+  final FlutterSecureStorage _secureStorage;
+
+  CacheHelper(this._prefs, this._secureStorage);
+
+  @override
+  Future<bool> saveData({required String key, required dynamic value}) async {
+    if (value is int) return await _prefs.setInt(key, value);
+    if (value is double) return await _prefs.setDouble(key, value);
+    if (value is String) return await _prefs.setString(key, value);
+    if (value is bool) return await _prefs.setBool(key, value);
+    if (value is List<String>) return await _prefs.setStringList(key, value);
+    return false;
+  }
+
+  @override
+  dynamic getData({required String key}) {
+    return _prefs.get(key);
+  }
+
+  @override
+  Future<bool> removeData({required String key}) async {
+    return await _prefs.remove(key);
+  }
+
+  @override
+  Future<void> saveSecureData({
+    required String key,
+    required String value,
+  }) async {
+    await _secureStorage.write(key: key, value: value);
+  }
+
+  @override
+  Future<String?> getSecureData(String key) async {
+    return await _secureStorage.read(key: key);
+  }
+
+  @override
+  Future<void> removeSecureData({required String key}) async {
+    await _secureStorage.delete(key: key);
+  }
+}
