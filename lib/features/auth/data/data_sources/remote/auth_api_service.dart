1be9d66bcaab77b34606015165a3a4c579ffd40a
Abdallah Tarek <badatarek1332003@gmail.com>
2026-04-10 13:46:55 +0200

    create auth api service login and signup

---
+++ lib/features/auth/data/data_sources/remote/auth_api_service.dart
@@ -0,0 +1,22 @@
+import 'package:dio/dio.dart';
+import 'package:e_commerce/core/network/api_constants.dart';
+import 'package:e_commerce/features/auth/data/models/login_response_model.dart';
+import 'package:e_commerce/features/auth/data/models/user_model.dart';
+import 'package:retrofit/retrofit.dart';
+
+
+part 'auth_api_service.g.dart';
+
+@RestApi(baseUrl: ApiConstants.baseUrl)
+abstract class AuthApiService {
+  factory AuthApiService(Dio dio, {String baseUrl}) = _AuthApiService;
+
+  @POST(ApiConstants.loginEndpoint)
+  Future<LoginResponseModel> login(@Body() Map<String, dynamic> body);
+
+  @POST(ApiConstants.signUpEndpoint)
+  Future<UserModel> signUp(@Body() Map<String, dynamic> body);
+
+  @GET(ApiConstants.profileEndpoint)
+  Future<UserModel> getProfile();
+}
