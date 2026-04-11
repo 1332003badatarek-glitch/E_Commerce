ab89ccedb10cc609cc7d315ee826d25db1417741
Abdallah Tarek <badatarek1332003@gmail.com>
2026-04-11 16:49:57 +0200

    create routing system

---
+++ lib/core/routing/app_router.dart
@@ -0,0 +1,21 @@
+import 'package:e_commerce/features/home/presentation/views/home_view.dart';
+import 'package:flutter/material.dart';
+import 'package:e_commerce/core/routing/routes.dart';
+import 'package:e_commerce/features/auth/presentation/views/login_view.dart';
+
+class AppRouter {
+  Route generateRoute(RouteSettings settings) {
+    switch (settings.name) {
+      case Routes.loginView:
+        return MaterialPageRoute(builder: (_) => const LoginView());
+      case Routes.homeView:
+        return MaterialPageRoute(builder: (_) => const HomeView());
+      default:
+        return MaterialPageRoute(
+          builder: (_) => Scaffold(
+            body: Center(child: Text('No route defined for ${settings.name}')),
+          ),
+        );
+    }
+  }
+}
