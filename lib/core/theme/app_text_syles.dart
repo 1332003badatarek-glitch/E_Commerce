5774e9e937f7bf1999f2d3c172f75b8937d3fd55
Abdallah Tarek <badatarek1332003@gmail.com>
2026-04-11 16:51:44 +0200

    optmiaz theme data class

--- lib/core/theme/app_text_syles.dart
+++ lib/core/theme/app_text_syles.dart
@@ -1,68 +1,92 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:e_commerce/core/theme/app_colors.dart';
 
 class AppTextStyles {
   static const String _fontFamily = 'Montserrat';
 
-  static TextStyle font24BoldBlack = TextStyle(
-    fontSize: 24.sp,
+  static TextStyle font36BoldBlack = TextStyle(
+    fontSize: 36,
     fontWeight: FontWeight.w700,
     color: AppColors.black,
     fontFamily: _fontFamily,
   );
+  static TextStyle font20SimiBoldWhite = TextStyle(
+    fontSize: 20,
+    fontWeight: FontWeight.w600,
+    color: AppColors.white,
+    fontFamily: _fontFamily,
+  );
+  static TextStyle font14SimiBoldWhite = TextStyle(
+    fontSize: 14,
+    fontWeight: FontWeight.w600,
+    color: AppColors.white,
+    fontFamily: _fontFamily,
+  );
 
   static TextStyle font18SemiBoldBlack = TextStyle(
-    fontSize: 18.sp,
+    fontSize: 18,
     fontWeight: FontWeight.w600,
     color: AppColors.black,
     fontFamily: _fontFamily,
   );
 
   static TextStyle font12MediumBlack = TextStyle(
-    fontSize: 12.sp,
+    fontSize: 12,
     fontWeight: FontWeight.w500,
     color: AppColors.black,
     fontFamily: _fontFamily,
   );
 
   static TextStyle font12RegularGrey = TextStyle(
-    fontSize: 12.sp,
+    fontSize: 12,
     fontWeight: FontWeight.w400,
+    color: AppColors.darkGrey,
+    fontFamily: _fontFamily,
+  );
+  static TextStyle font12MediumGrey = TextStyle(
+    fontSize: 12,
+    fontWeight: FontWeight.w500,
+    color: AppColors.darkGrey,
+    fontFamily: _fontFamily,
+  );
+
+  static TextStyle font14SemiBoldGrey = TextStyle(
+    fontSize: 14,
+    fontWeight: FontWeight.w600,
     color: AppColors.darkGrey,
     fontFamily: _fontFamily,
   );
 
   static TextStyle font16SemiBoldWhite = TextStyle(
-    fontSize: 16.sp,
+    fontSize: 16,
     fontWeight: FontWeight.w600,
     color: AppColors.white,
     fontFamily: _fontFamily,
   );
 
   static TextStyle font12SemiBoldPrimary = TextStyle(
-    fontSize: 12.sp,
+    fontSize: 12,
     fontWeight: FontWeight.w600,
     color: AppColors.primary,
     fontFamily: _fontFamily,
   );
 
   static TextStyle font12RegularHintGrey = TextStyle(
-    fontSize: 12.sp,
+    fontSize: 12,
     fontWeight: FontWeight.w400,
     color: AppColors.darkGrey,
     fontFamily: _fontFamily,
   );
 
   static TextStyle font14SemiBoldBlack = TextStyle(
-    fontSize: 14.sp,
+    fontSize: 14,
     fontWeight: FontWeight.w600,
     color: AppColors.black,
     fontFamily: _fontFamily,
   );
 
   static TextStyle font12RegularOldPriceGrey = TextStyle(
-    fontSize: 12.sp,
+    fontSize: 12,
     fontWeight: FontWeight.w400,
     color: AppColors.discountPrice,
     decoration: TextDecoration.lineThrough,
@@ -70,7 +94,7 @@ class AppTextStyles {
   );
 
   static TextStyle font10RegularDiscountOrange = TextStyle(
-    fontSize: 10.sp,
+    fontSize: 10,
     fontWeight: FontWeight.w400,
     color: AppColors.discountPercent,
     fontFamily: _fontFamily,
---
+++ lib/core/theme/app_theme.dart
@@ -0,0 +1,92 @@
+import 'package:e_commerce/core/theme/app_text_syles.dart';
+import 'package:flutter/material.dart';
+import 'app_colors.dart';
+
+class AppTheme {
+  static ThemeData get lightTheme {
+    return ThemeData(
+      useMaterial3: true,
+      primaryColor: AppColors.primary,
+      scaffoldBackgroundColor: AppColors.white,
+      brightness: Brightness.light,
+      fontFamily: 'Montserrat',
+
+      textTheme: TextTheme(
+        displayLarge: AppTextStyles.font36BoldBlack,
+        headlineLarge: AppTextStyles.font36BoldBlack,
+
+        headlineMedium: AppTextStyles.font18SemiBoldBlack,
+
+        titleMedium: AppTextStyles.font14SemiBoldBlack,
+
+        bodyLarge: AppTextStyles.font12MediumBlack,
+
+        bodyMedium: AppTextStyles.font12RegularGrey,
+
+        labelSmall: AppTextStyles.font10RegularDiscountOrange,
+        titleSmall: AppTextStyles.font14SimiBoldWhite,
+      ),
+
+      appBarTheme: AppBarTheme(
+        backgroundColor: AppColors.white,
+        elevation: 0,
+        centerTitle: true,
+        titleTextStyle: AppTextStyles.font18SemiBoldBlack,
+        iconTheme: const IconThemeData(color: AppColors.black),
+      ),
+
+      colorScheme: ColorScheme.fromSeed(
+        seedColor: AppColors.primary,
+        primary: AppColors.primary,
+        onPrimary: AppColors.white,
+        surface: AppColors.white,
+        error: AppColors.primary,
+      ),
+
+      inputDecorationTheme: InputDecorationTheme(
+        isDense: true,
+        contentPadding: EdgeInsets.symmetric(horizontal: 20, vertical: 20),
+        filled: true,
+        fillColor: AppColors.customWhite,
+
+        enabledBorder: OutlineInputBorder(
+          borderSide: BorderSide(color: AppColors.darkGrey, width: 1.3),
+          borderRadius: BorderRadius.circular(10),
+        ),
+
+        focusedBorder: OutlineInputBorder(
+          borderSide: const BorderSide(color: AppColors.primary, width: 1.3),
+          borderRadius: BorderRadius.circular(10),
+        ),
+        errorBorder: OutlineInputBorder(
+          borderSide: const BorderSide(color: AppColors.red, width: 1.3),
+
+          borderRadius: BorderRadius.circular(10),
+        ),
+        errorStyle: AppTextStyles.font12SemiBoldPrimary.copyWith(
+          color: AppColors.red,
+        ),
+        focusedErrorBorder: OutlineInputBorder(
+          borderSide: const BorderSide(color: AppColors.red, width: 1.3),
+
+          borderRadius: BorderRadius.circular(10),
+        ),
+        hintStyle: AppTextStyles.font14SemiBoldGrey,
+      ),
+
+      elevatedButtonTheme: ElevatedButtonThemeData(
+        style: ElevatedButton.styleFrom(
+          backgroundColor: AppColors.primary,
+          foregroundColor: AppColors.white,
+
+          minimumSize: Size(double.infinity, 55),
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(10),
+          ),
+          textStyle: AppTextStyles.font20SimiBoldWhite,
+          elevation: 0,
+        ),
+      ),
+    );
+  }
+}
