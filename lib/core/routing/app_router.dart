457fdf3fdf3b42080ff5b41cc0a091ee2ced2582
Abdallah Tarek <badatarek1332003@gmail.com>
2026-04-15 23:40:58 +0200

    providing the login cubit in router of login view

--- lib/core/routing/app_router.dart
+++ lib/core/routing/app_router.dart
@@ -1,4 +1,5 @@
 import 'package:e_commerce/core/di/service_locator.dart';
+import 'package:e_commerce/features/auth/presentation/cubits/login/login_cubit.dart';
 import 'package:e_commerce/features/auth/presentation/cubits/sign_up/sign_up_cubit.dart';
 import 'package:e_commerce/features/auth/presentation/views/sign_up_view.dart';
 import 'package:e_commerce/features/home/presentation/views/home_view.dart';
@@ -11,7 +12,12 @@ class AppRouter {
   Route generateRoute(RouteSettings settings) {
     switch (settings.name) {
       case Routes.loginView:
-        return MaterialPageRoute(builder: (_) => const LoginView());
+        return MaterialPageRoute(
+          builder: (_) => BlocProvider(
+            create: (context) => getIt<LoginCubit>(),
+            child: const LoginView(),
+          ),
+        );
       case Routes.homeView:
         return MaterialPageRoute(builder: (_) => const HomeView());
       case Routes.signUpView:
--- lib/main.dart
+++ lib/main.dart
@@ -2,9 +2,7 @@ import 'package:e_commerce/core/di/service_locator.dart';
 import 'package:e_commerce/core/routing/app_router.dart';
 import 'package:e_commerce/core/routing/routes.dart';
 import 'package:e_commerce/core/theme/app_theme.dart';
-import 'package:e_commerce/features/auth/presentation/cubits/login/login_cubit.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_bloc/flutter_bloc.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -18,16 +16,13 @@ class Stylish extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MultiBlocProvider(
-      providers: [BlocProvider(create: (context) => getIt<LoginCubit>())],
-      child: MaterialApp(
-        debugShowCheckedModeBanner: false,
-        title: 'E-commerce App',
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'E-commerce App',
 
-        theme: AppTheme.lightTheme,
-        initialRoute: Routes.loginView,
-        onGenerateRoute: appRouter.generateRoute,
-      ),
+      theme: AppTheme.lightTheme,
+      initialRoute: Routes.loginView,
+      onGenerateRoute: appRouter.generateRoute,
     );
   }
 }
