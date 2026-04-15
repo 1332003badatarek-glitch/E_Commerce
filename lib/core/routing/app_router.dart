fb0370e4c92e66ac43e34e1f6d04801a1894bcb0
Abdallah Tarek <badatarek1332003@gmail.com>
2026-04-15 18:48:18 +0200

    provid the signup cubit to the signup view

--- lib/core/routing/app_router.dart
+++ lib/core/routing/app_router.dart
@@ -1,8 +1,11 @@
+import 'package:e_commerce/core/di/service_locator.dart';
+import 'package:e_commerce/features/auth/presentation/cubits/sign_up/sign_up_cubit.dart';
 import 'package:e_commerce/features/auth/presentation/views/sign_up_view.dart';
 import 'package:e_commerce/features/home/presentation/views/home_view.dart';
 import 'package:flutter/material.dart';
 import 'package:e_commerce/core/routing/routes.dart';
 import 'package:e_commerce/features/auth/presentation/views/login_view.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
 class AppRouter {
   Route generateRoute(RouteSettings settings) {
@@ -12,7 +15,12 @@ class AppRouter {
       case Routes.homeView:
         return MaterialPageRoute(builder: (_) => const HomeView());
       case Routes.signUpView:
-        return MaterialPageRoute(builder: (_) => const SignUpView());
+        return MaterialPageRoute(
+          builder: (_) => BlocProvider(
+            create: (context) => getIt<SignUpCubit>(),
+            child: const SignUpView(),
+          ),
+        );
       default:
         return MaterialPageRoute(
           builder: (_) => Scaffold(
