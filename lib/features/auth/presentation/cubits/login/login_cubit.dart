7fdd6ffc0409ca3acd6825ac9a8800c854bef212
Abdallah Tarek <badatarek1332003@gmail.com>
2026-04-15 22:02:16 +0200

    edit login cubit to fit with the new edits

--- lib/features/auth/presentation/cubits/login/login_cubit.dart
+++ lib/features/auth/presentation/cubits/login/login_cubit.dart
@@ -1,4 +1,6 @@
+import 'package:e_commerce/core/use_cases/use_case_interface.dart';
 import 'package:e_commerce/features/auth/domain/entities/user_entity.dart';
+import 'package:e_commerce/features/auth/domain/use_cases/get_profile_use_case.dart';
 import 'package:e_commerce/features/auth/domain/use_cases/login_use_case.dart';
 import 'package:equatable/equatable.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
@@ -6,18 +8,26 @@ import 'package:flutter_bloc/flutter_bloc.dart';
 part 'login_state.dart';
 
 class LoginCubit extends Cubit<LoginState> {
-  LoginCubit(this._loginUseCase) : super(LoginInitial());
+  LoginCubit(this._loginUseCase, this._getProfileUseCase)
+    : super(LoginInitial());
 
   final LoginUseCase _loginUseCase;
+  final GetProfileUseCase _getProfileUseCase;
 
   Future<void> login({required String email, required String password}) async {
     emit(LoginLoading());
-    final result = await _loginUseCase(
+    final loginResult = await _loginUseCase(
       LoginParams(email: email, password: password),
     );
-    result.fold(
-      (failure) => emit(LoginFailure(erorrMessage: failure.message)),
-      (user) => emit(LoginSuccess(userEntity: user)),
+    await loginResult.fold(
+      (failure) async => emit(LoginFailure(erorrMessage: failure.message)),
+      (_) async {
+        final userProfileResult = await _getProfileUseCase(NoParams());
+        userProfileResult.fold(
+          (failure) => emit(LoginFailure(erorrMessage: failure.message)),
+          (user) => emit(LoginSuccess(userEntity: user)),
+        );
+      },
     );
   }
 }
