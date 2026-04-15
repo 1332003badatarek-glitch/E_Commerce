4d1ecb2323ecbb4e896567d5b899b98b93cea2ee
Abdallah Tarek <badatarek1332003@gmail.com>
2026-04-15 18:47:38 +0200

    create signup view and signup form then inject the signup cubit

--- lib/features/auth/presentation/views/sign_up_view.dart
+++ lib/features/auth/presentation/views/sign_up_view.dart
@@ -1,11 +1,51 @@
+import 'package:e_commerce/core/helpers/app_helper_functions.dart';
+import 'package:e_commerce/core/routing/routes.dart';
+import 'package:e_commerce/features/auth/presentation/cubits/sign_up/sign_up_cubit.dart';
+import 'package:e_commerce/features/auth/presentation/widgets/sign_up_form.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
 class SignUpView extends StatelessWidget {
   const SignUpView({super.key});
-
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
+    return BlocListener<SignUpCubit, SignUpState>(
+      listenWhen: (previous, current) =>
+          current is SignUpLoading ||
+          current is SignUpSuccess ||
+          current is SignUpFailure,
+
+      listener: (context, state) {
+        if (state is SignUpLoading) {
+          AppHelperFunctions.loadingDialog(context);
+        } else if (state is SignUpSuccess) {
+          Navigator.pop(context);
+          Navigator.pushNamed(context, Routes.loginView);
+        } else if (state is SignUpFailure) {
+          Navigator.pop(context);
+          AppHelperFunctions.showFailure(context, state.erorrMessage);
+        }
+      },
+      child: Scaffold(
+        body: SafeArea(
+          child: SingleChildScrollView(
+            child: Padding(
+              padding: EdgeInsets.symmetric(horizontal: 22, vertical: 22),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(
+                    'Create an\nAccount',
+                    style: Theme.of(context).textTheme.displayLarge,
+                  ),
+                  SizedBox(height: 30),
+                  SignUpForm(),
+                ],
+              ),
+            ),
+          ),
+        ),
+      ),
     );
   }
 }
---
+++ lib/features/auth/presentation/widgets/sign_up_form.dart
@@ -0,0 +1,82 @@
+import 'dart:io';
+
+import 'package:e_commerce/core/helpers/app_validators.dart';
+import 'package:e_commerce/features/auth/presentation/cubits/sign_up/sign_up_cubit.dart';
+import 'package:e_commerce/features/auth/presentation/widgets/auth_button.dart';
+import 'package:e_commerce/features/auth/presentation/widgets/custom_tex_form_field.dart';
+import 'package:e_commerce/features/auth/presentation/widgets/image_picker_widget.dart';
+import 'package:e_commerce/features/auth/presentation/widgets/password_text_field.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+
+class SignUpForm extends StatefulWidget {
+  const SignUpForm({super.key});
+
+  @override
+  State<SignUpForm> createState() => _LoginFormState();
+}
+
+class _LoginFormState extends State<SignUpForm> {
+  final _formKey = GlobalKey<FormState>();
+  final _nameController = TextEditingController();
+  final _emailController = TextEditingController();
+  final _passwordController = TextEditingController();
+  File? _selectedImage;
+
+  @override
+  void dispose() {
+    _nameController.dispose();
+    _emailController.dispose();
+    _passwordController.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Form(
+      key: _formKey,
+      child: Column(
+        children: [
+          ImagePickerWidget(
+            onImagePicked: (image) {
+              _selectedImage = image;
+            },
+          ),
+          CustomTextFormField(
+            controller: _nameController,
+            hintText: 'Name',
+            prefixIcon: Icons.person,
+            validator: (value) => AppValidators.validateRequired(value, 'Name'),
+          ),
+          SizedBox(height: 20),
+
+          CustomTextFormField(
+            controller: _emailController,
+            hintText: 'Username or Email',
+            prefixIcon: Icons.email,
+            validator: (value) => AppValidators.validateEmail(value),
+          ),
+          SizedBox(height: 20),
+          PasswordTextField(
+            controller: _passwordController,
+            validator: (value) => AppValidators.validatePassword(value),
+          ),
+          SizedBox(height: 40),
+          AuthButton(
+            onPressed: () {
+              if (_formKey.currentState!.validate()) {
+                context.read<SignUpCubit>().signUp(
+                  name: _nameController.text.trim(),
+                  email: _emailController.text.trim(),
+                  password: _passwordController.text,
+                  imageFile: _selectedImage, // تم الاستخدام بنجاح!
+                );
+              }
+            },
+            title: 'SignUp',
+          ),
+        ],
+      ),
+    );
+  }
+}
