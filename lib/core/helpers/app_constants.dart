c73e7ff8000f15db01a3762ce258a0c0c4793c05
Abdallah Tarek <badatarek1332003@gmail.com>
2026-04-23 18:10:52 +0200

    add app constants file to core folder

---
+++ lib/core/helpers/app_constants.dart
@@ -0,0 +1,5 @@
+class AppConstants {
+  static const String appName = 'Eshop';
+  static const String appLogo = 'assets/images/app_logo.png';
+  static const String userAvatar = 'assets/images/user_avatar.jpg';
+}
