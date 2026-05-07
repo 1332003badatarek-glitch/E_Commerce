9df98c336044e34353364e222f98065f66028f14
Abdallah Tarek <badatarek1332003@gmail.com>
2026-05-07 17:00:38 +0300

    refactor categories ui

--- lib/features/categories/presentation/widgets/categories_list_view.dart
+++ lib/features/categories/presentation/widgets/categories_list_view.dart
@@ -1,6 +1,6 @@
 import 'package:e_commerce/features/categories/presentation/cubits/categories/categories_cubit.dart';
+import 'package:e_commerce/features/categories/presentation/widgets/categories_loading_list.dart';
 import 'package:e_commerce/features/categories/presentation/widgets/category_item.dart';
-import 'package:e_commerce/features/categories/presentation/widgets/category_shimmer_loading.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
@@ -13,7 +13,7 @@ class CategoriesListView extends StatelessWidget {
       builder: (context, state) {
         if (state is CategoriesSuccess) {
           return SizedBox(
-            height: 110,
+            height: 115,
             child: ListView.builder(
               itemCount: state.categories.length,
               scrollDirection: Axis.horizontal,
@@ -29,21 +29,9 @@ class CategoriesListView extends StatelessWidget {
         } else if (state is CategoriesFailure) {
           return Center(child: Text(state.errorMessage));
         } else {
-          // حالة الـ Loading
           return SizedBox(
             height: 150,
-            child: ListView.builder(
-              itemCount: 5, // بنعرض 5 عناصر وهمية لحد ما الداتا تيجي
-              scrollDirection: Axis.horizontal,
-              physics:
-                  NeverScrollableScrollPhysics(), // بنوقف السكرول أثناء اللودينج
-              itemBuilder: (context, index) {
-                return const Padding(
-                  padding: EdgeInsets.symmetric(horizontal: 8.0),
-                  child: CategoryShimmerLoading(),
-                );
-              },
-            ),
+            child: CategoriesLoadingList(),
           );
         }
       },
